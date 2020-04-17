5a1bdef170b4d2284131c2bccf8898a45c5d7a42
happa64 <54537005+happa64@users.noreply.github.com>
2020-04-17 11:08:11 +0900

    init commit

---
+++ ABC007/ABC007-A.py
@@ -0,0 +1,16 @@
+# https://atcoder.jp/contests/abc007/tasks/abc007_1
+# A - 植木算
+import sys
+
+sys.setrecursionlimit(10 ** 7)
+f_inf = float('inf')
+mod = 10 ** 9 + 7
+
+
+def resolve():
+    n = int(input())
+    print(n - 1)
+
+
+if __name__ == '__main__':
+    resolve()
