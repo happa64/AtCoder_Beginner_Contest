bc0b5e99a9d1f69fdff8d44a9396b8c2496684d7
happa64 <54537005+happa64@users.noreply.github.com>
2020-05-11 01:54:33 +0900

    init commit

---
+++ ABC/ABC167/ABC167-D.py
@@ -0,0 +1,46 @@
+# https://atcoder.jp/contests/abc167/submissions/13099757
+# D - Teleporter
+import sys
+
+sys.setrecursionlimit(10 ** 7)
+f_inf = float('inf')
+mod = 10 ** 9 + 7
+
+
+def resolve():
+    n, k = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    tree = [[] for _ in range(n)]
+    for idx, a in enumerate(A):
+        tree[idx].append(a - 1)
+
+    # 1からの距離をメモしながらテレポートし、既に訪れた場所にテレポートしたら、1と閉路の開始点、終了点の各距離を配列に入れる
+    def dfs(pre, dist):
+        depth[pre] = dist
+        for to in tree[pre]:
+            if depth[to] != f_inf:
+                cycle.append([depth[to], depth[pre] + 1])
+                return
+            else:
+                dfs(to, dist + 1)
+
+    cycle = []
+    depth = [f_inf for _ in range(n)]
+    dfs(0, 0)
+
+    cycle_start = cycle[0][0]
+    cycle_end = cycle[0][1]
+    # kが閉路までの距離以下であれば、1から距離kの位置を出力
+    if k <= cycle_start:
+        print(depth.index(k) + 1)
+    else:
+        # kが閉路までの距離より大きければ、1から最終的な町の距離は
+        # 1から閉路までの距離 +（k - 閉路までの距離）%（閉路の長さ）である
+        remainder = (k - cycle_start) % (cycle_end - cycle_start)
+        res = cycle_start + remainder
+        print(depth.index(res) + 1)
+
+
+if __name__ == '__main__':
+    resolve()
--- Library/Combination _Mod.py
+++ Library/Combination _Mod.py
@@ -1,17 +1,16 @@
 # 二項係数(mod p)をO(1)で求める
-class ModComb:
-    def __init__(self, MAX, mod=10 ** 9 + 7):
-        fac = [1, 1]
-        finv = [1, 1]
-        inv = [0, 1]
-        for i in range(2, MAX):
-            fac.append(fac[i - 1] * i % mod)
-            inv.append(mod - inv[mod % i] * (mod // i) % mod)
-            finv.append(finv[i - 1] * inv[i] % mod)
-        self.fac, self.finv, self.mod = fac, finv, mod
+def cmb(n, r, mod):
+    if (r < 0) or (n < r):
+        return 0
+    r = min(r, n - r)
+    return fact[n] * factinv[r] * factinv[n - r] % mod
 
-    def nCk(self, n, k):
-        if n < k or n < 0 or k < 0:
-            return 0
-        fac, finv, mod = self.fac, self.finv, self.mod
-        return fac[n] * (finv[k] * finv[n - k] % mod) % mod
+
+fact = [1, 1]
+factinv = [1, 1]
+inv = [0, 1]
+
+for i in range(2, n + 1):
+    fact.append((fact[-1] * i) % mod)
+    inv.append((-inv[mod % i] * (mod // i)) % mod)
+    factinv.append((factinv[-1] * inv[-1]) % mod)
