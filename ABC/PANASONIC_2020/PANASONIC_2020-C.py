27f97697a7774717f8575fb58d8c6b3cfd493d6f
HPP <happa_iidx@yahoo.co.jp>
2020-06-01 18:46:47 +0900

    init commit

--- ABC/PANASONIC_2020/PANASONIC_2020-C.py
+++ ABC/PANASONIC_2020/PANASONIC_2020-C.py
@@ -10,7 +10,7 @@ mod = 10 ** 9 + 7
 
 def resolve():
     a, b, c = input().split()
-    print("Yes" if Decimal(a) ** Decimal(0.5) + Decimal(b) ** Decimal(0.5) < Decimal(c) ** Decimal(0.5) else "No")
+    print("Yes" if Decimal(a) ** Decimal("0.5") + Decimal(b) ** Decimal("0.5") < Decimal(c) ** Decimal("0.5") else "No")
 
 
 if __name__ == '__main__':
