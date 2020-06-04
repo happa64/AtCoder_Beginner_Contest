00f60ab8a6eb269959074dac4ce811137445980f
HPP <happa_iidx@yahoo.co.jp>
2020-06-04 16:26:54 +0900

    二分探索をクラス化

--- ABC/ABC020/ABC020-C.py
+++ ABC/ABC020/ABC020-C.py
@@ -1,4 +1,4 @@
-# https://atcoder.jp/contests/abc020/submissions/13943212
+# https://atcoder.jp/contests/abc020/submissions/13998312
 # C - 壁抜け
 import sys
 from collections import deque
@@ -8,18 +8,25 @@ f_inf = float('inf')
 mod = 10 ** 9 + 7
 
 
+class BinarySearch:
+    def meguru_bisect(self, is_ok, left, right):
+        """
+        :param is_ok: 定義して下さい
+        :param left: 取りうる最小の値-1
+        :param right: 取りうる最大の値+1
+        :return: is_okを満たす最小(もしくは最大)の値
+        """
+        while abs(left - right) > 1:
+            mid = (left + right) // 2
+            if is_ok(mid):
+                left = mid
+            else:
+                right = mid
+        return left
+
+
 def resolve():
-    H, W, T = map(int, input().split())
-    grid = [list(input()) for _ in range(H)]
-
-    for h in range(H):
-        for w in range(W):
-            if grid[h][w] == "S":
-                sh, sw = h, w
-            elif grid[h][w] == "G":
-                gh, gw = h, w
-
-    def is_ok(x):
+    def bfs(x):
         """
         幅優先探索でSからGまで向かう最小時間を計算し、それがT秒以内かどうかを返す
         :param x: 黒マスに移動するコスト
@@ -33,8 +40,7 @@ def resolve():
                 next_h, next_w = h + dh, w + dw
                 if next_h < 0 or next_h >= H or next_w < 0 or next_w >= W:
                     continue
-
-                if grid[next_h][next_w] == "." or grid[next_h][next_w] == "G" or grid[next_h][next_w] == "S":
+                if grid[next_h][next_w] == "." or grid[next_h][next_w] == "G":
                     if dp[next_h][next_w] > dp[h][w] + 1:
                         dp[next_h][next_w] = dp[h][w] + 1
                         que.append([next_h, next_w])
@@ -44,24 +50,18 @@ def resolve():
                         que.append([next_h, next_w])
         return dp[gh][gw] <= T
 
-    def meguru_bisect(ng, ok):
-        """
-        初期値のng,okを受け取り,is_okを満たす最小(最大)のokを返す
-        まずis_okを定義すべし
-        ng ok は  とり得る最小の値-1 とり得る最大の値+1
-        最大最小が逆の場合はよしなにひっくり返す
-        """
-        while abs(ok - ng) > 1:
-            mid = (ok + ng) // 2
-            if is_ok(mid):
-                ok = mid
-            else:
-                ng = mid
-        return ok
+    H, W, T = map(int, input().split())
+    grid = [list(input()) for _ in range(H)]
 
-    ok = 0
-    ng = 10 ** 9
-    res = meguru_bisect(ng, ok)
+    for h in range(H):
+        for w in range(W):
+            if grid[h][w] == "S":
+                sh, sw = h, w
+            elif grid[h][w] == "G":
+                gh, gw = h, w
+
+    bs = BinarySearch()
+    res = bs.meguru_bisect(bfs, 0, 10 ** 9)
     print(res)
 
 
--- Library/Binary_Search.py
+++ Library/Binary_Search.py
@@ -1,19 +1,16 @@
-# めぐる式二分探索法改
-def is_ok(arg):
-    return  # 条件式
+class BinarySearch:
+    def meguru_bisect(self, is_ok, left, right):
+        """
+        :param is_ok: 定義して下さい
+        :param left: 取りうる最小の値-1
+        :param right: 取りうる最大の値+1
+        :return: is_okを満たす最小(もしくは最大)の値
+        """
+        while abs(left - right) > 1:
+            mid = (left + right) // 2
+            if is_ok(mid):
+                left = mid
+            else:
+                right = mid
+        return left
 
-
-def meguru_bisect(ng, ok):
-    """
-    初期値のng,okを受け取り,is_okを満たす最小(最大)のokを返す
-    まずis_okを定義すべし
-    ng ok は  とり得る最小の値-1 とり得る最大の値+1
-    最大最小が逆の場合はよしなにひっくり返す
-    """
-    while abs(ok - ng) > 1:
-        mid = (ok + ng) // 2
-        if is_ok(mid):
-            ok = mid
-        else:
-            ng = mid
-    return ok
