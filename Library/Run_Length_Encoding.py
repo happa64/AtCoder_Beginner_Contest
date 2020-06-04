3a02fb781492e708dd3de0adb0bce34d0c55bc2b
HPP <happa_iidx@yahoo.co.jp>
2020-06-04 16:55:33 +0900

    クラス化しました デコード機能を追加しました

--- Library/Run_Length_Encoding.py
+++ Library/Run_Length_Encoding.py
@@ -1,10 +1,16 @@
 class RunLengthEncoding:
     def __init__(self, s):
+        """
+        :param s:エンコードもしくはデコードしたい文字列
+        """
         self.s = s
         self.ans = ""
         self.count = 0
 
     def encoding(self):
+        """
+        :return:連長圧縮した文字列を返します
+        """
         tmp = self.s[0]
         for i in range(len(s)):
             if tmp == s[i]:
@@ -15,3 +21,20 @@ class RunLengthEncoding:
                 tmp = s[i]
         return self.ans + tmp + str(self.count)
 
+    def decoding(self):
+        """
+        :return:連長圧縮された文字列を元に戻した文字列を返します
+        """
+        tmp = self.s[0]
+        num = ""
+        i = 1
+        while i < len(self.s):
+            while i < len(self.s) and self.s[i].isdigit():
+                num += self.s[i]
+                i += 1
+            self.ans += tmp * int(num)
+            if i < len(self.s):
+                tmp = self.s[i]
+                num = ""
+                i += 1
+        return self.ans
