48fdd24b1ae89c180147ae14b4dc4044d3c68be5
HPP <happa_iidx@yahoo.co.jp>
2020-06-05 23:25:56 +0900

    init commit

--- Library/Binary_Search.py
+++ Library/Binary_Search.py
@@ -1,16 +1,14 @@
-class BinarySearch:
-    def meguru_bisect(self, is_ok, left, right):
-        """
-        :param is_ok: 定義して下さい
-        :param left: 取りうる最小の値-1
-        :param right: 取りうる最大の値+1
-        :return: is_okを満たす最小(もしくは最大)の値
-        """
-        while abs(left - right) > 1:
-            mid = (left + right) // 2
-            if is_ok(mid):
-                left = mid
-            else:
-                right = mid
-        return left
-
+def meguru_bisect(left, right):
+    """
+    is_okを定義して下さい
+    :param left: 取りうる最小の値-1
+    :param right: 取りうる最大の値+1
+    :return: is_okを満たす最小(もしくは最大)の値
+    """
+    while abs(left - right) > 1:
+        mid = (left + right) // 2
+        if is_ok(mid):
+            right = mid
+        else:
+            left = mid
+    return right
