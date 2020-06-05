c279ea08df163ac8ad6d998d19ff61ac7f05f682
HPP <happa_iidx@yahoo.co.jp>
2020-06-05 19:42:41 +0900

    scipyを使った解法を追記

---
+++ AGC/CODE_FESTIVAL_2016_Qual_C/CODE_FESTIVAL_2016_Qual_C-C.py
@@ -0,0 +1,45 @@
+# https://atcoder.jp/contests/code-festival-2016-qualc/submissions/14026377
+# C - 二人のアルピニスト
+import sys
+
+sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
+f_inf = float('inf')
+mod = 10 ** 9 + 7
+
+
+def resolve():
+    n = int(input())
+    T = list(map(int, input().split()))
+    A = list(map(int, input().split()))
+
+    pattern = [0] * n
+    prev = -f_inf
+    for i in range(n):
+        if prev < T[i]:
+            pattern[i] = 1
+            prev = T[i]
+
+    prev = -f_inf
+    for i in reversed(range(n)):
+        if prev < A[i]:
+            if pattern[i] != 0 and T[i] != A[i]:
+                print(0)
+                exit()
+            pattern[i] = 1
+            prev = A[i]
+        else:
+            if pattern[i] != 0 and T[i] > A[i]:
+                print(0)
+                exit()
+
+    res = 1
+    for i in range(n):
+        if pattern[i] == 0:
+            pattern[i] = min(T[i], A[i])
+        res = res * pattern[i] % mod
+    print(res)
+
+
+if __name__ == '__main__':
+    resolve()
