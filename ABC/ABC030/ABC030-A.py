788e1d28d500c3840c075753640a34404980820f
HPP <happa_iidx@yahoo.co.jp>
2020-06-09 11:09:42 +0900

    init commit

---
+++ ABC/ABC030/ABC030-A.py
@@ -0,0 +1,15 @@
+# https://atcoder.jp/contests/abc029/submissions/14127333
+# A - 複数形
+import sys
+
+sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
+f_inf = float('inf')
+mod = 10 ** 9 + 7
+
+
+def resolve():
+
+
+if __name__ == '__main__':
+    resolve()
