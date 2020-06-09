c8338e9d9bcbaa8b761966e973473b5762507ab6
HPP <happa_iidx@yahoo.co.jp>
2020-06-09 12:19:19 +0900

    init commit

---
+++ ABC/ABC098/ABC098-D.py
@@ -0,0 +1,32 @@
+# https://atcoder.jp/contests/abc098/submissions/14128430
+# D - Xor Sum 2
+import sys
+
+sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
+f_inf = float('inf')
+mod = 10 ** 9 + 7
+
+
+def resolve():
+    n = int(input())
+    A = list(map(int, input().split()))
+
+    res = 0
+    left = 0
+    add_total = 0
+    xor_total = 0
+    for right in range(n):
+        add_total += A[right]
+        xor_total ^= A[right]
+        while add_total != xor_total:
+            add_total -= A[left]
+            xor_total ^= A[left]
+            left += 1
+        res += right - left + 1
+
+    print(res)
+
+
+if __name__ == '__main__':
+    resolve()
