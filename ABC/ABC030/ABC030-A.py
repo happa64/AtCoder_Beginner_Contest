7f2136f08e9f03f907ae8d07996220e3e32d6ac4
HPP <happa_iidx@yahoo.co.jp>
2020-06-09 11:15:09 +0900

    init commit

--- ABC/ABC030/ABC030-A.py
+++ ABC/ABC030/ABC030-A.py
@@ -1,5 +1,5 @@
-# https://atcoder.jp/contests/abc029/submissions/14127333
-# A - 複数形
+# https://atcoder.jp/contests/abc030/submissions/14127402
+# A - 勝率計算
 import sys
 
 sys.setrecursionlimit(10 ** 7)
@@ -9,6 +9,8 @@ mod = 10 ** 9 + 7
 
 
 def resolve():
+    a, b, c, d = map(int, input().split())
+    print("TAKAHASHI" if b / a > d / c else "AOKI" if d / c > b / a else "DRAW" )
 
 
 if __name__ == '__main__':
