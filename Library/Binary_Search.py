e8e3bf04a67579881baa52a7865dc31b7e1089cf
HPP <happa_iidx@yahoo.co.jp>
2020-06-10 17:17:47 +0900

    init commit

--- Library/Binary_Search.py
+++ Library/Binary_Search.py
@@ -5,10 +5,27 @@ def meguru_bisect(ok, ng):
     :param ng: 取りうる最大の値+1
     :return: is_okを満たす最小(もしくは最大)の値
     """
-    while abs(ok - ng) > 10 ** (-7):
-        mid = (ok + ng) / 2
+    while abs(ok - ng) > 0:
+        mid = (ok + ng) // 2
         if is_ok(mid):
             ok = mid
         else:
             ng = mid
     return ok
+
+
+def ternary_search(high, low):
+    """
+    f=凸関数の式
+    :param high:取りうる一番大きい値
+    :param low:取りうる一番小さい値
+    :return: 凸関数の極小値（極大値）
+    """
+    while abs(high - low) > 10 ** (-9):
+        mid_left = high / 3 + low * 2 / 3
+        mid_right = high * 2 / 3 + low / 3
+        if f(mid_left) >= f(mid_right):
+            low = mid_left
+        else:
+            high = mid_right
+    return f(high)
