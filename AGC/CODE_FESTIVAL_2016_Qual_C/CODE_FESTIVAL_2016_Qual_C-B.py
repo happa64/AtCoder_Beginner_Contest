381ffc3b90ebc753747359d23a2d8522ee3c35d4
HPP <happa_iidx@yahoo.co.jp>
2020-06-14 13:57:51 +0900

    より高速な解法に変更

--- AGC/CODE_FESTIVAL_2016_Qual_C/CODE_FESTIVAL_2016_Qual_C-B.py
+++ AGC/CODE_FESTIVAL_2016_Qual_C/CODE_FESTIVAL_2016_Qual_C-B.py
@@ -1,8 +1,9 @@
-# https://atcoder.jp/contests/code-festival-2016-qualc/tasks/codefestival_2016_qualC_b
+# https://atcoder.jp/contests/code-festival-2016-qualc/submissions/14271793
 # B - K個のケーキ
 import sys
 
 sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
 f_inf = float('inf')
 mod = 10 ** 9 + 7
 
@@ -10,39 +11,8 @@ mod = 10 ** 9 + 7
 def resolve():
     k, t = map(int, input().split())
     A = list(map(int, input().split()))
-
-    # ケーキの種類iとケーキの個数aiの配列を作り、ケーキの個数順に昇順ソートする
-    B = []
-    for i in range(t):
-        B.append([i + 1, A[i]])
-    B = sorted(B, key=lambda x: x[1])
-
-    # ケーキの種類が1つしかなければ、日にち-1が答え
-    if t == 1:
-        print(k - 1)
-        exit()
-
-    res = 0
-    # pre：前回食べたケーキ
-    pre = 0
-    for i in range(k):
-        # 一番多いケーキが前回食べたケーキでなければ、一番多いケーキを食べる
-        if pre != B[-1][0]:
-            pre = B[-1][0]
-            B[-1][1] -= 1
-            B = sorted(B, key=lambda x: x[1])
-        else:
-            # 前回食べたケーキが一番多いケーキであれば、二番目に多いケーキを食べる
-            if B[-2][1] != 0:
-                pre = B[-2][0]
-                B[-2][1] -= 1
-                B = sorted(B, key=lambda x: x[1])
-
-            # 前回食べたケーキが一番多いケーキであり、二番目に多いケーキがもうなければ、前回食べたケーキを食べる
-            else:
-                B[-1][1] -= 1
-                res += 1
-
+    etc = sum(A) - max(A)
+    res = max(0, max(A) - 1 - etc)
     print(res)
 
 
