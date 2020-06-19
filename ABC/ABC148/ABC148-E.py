a95f6f8b092f275b60a1e3c2b65413c34a689f89
HPP <happa_iidx@yahoo.co.jp>
2020-06-19 10:59:00 +0900

    init commit

--- ABC/ABC148/ABC148-E.py
+++ ABC/ABC148/ABC148-E.py
@@ -1,3 +1,5 @@
+# https://atcoder.jp/contests/abc148/submissions/14463483
+# E - Double Factorial
 import sys
 
 sys.setrecursionlimit(10 ** 7)
@@ -9,11 +11,15 @@ mod = 10 ** 9 + 7
 def resolve():
     n = int(input())
 
+    if n % 2 != 0:
+        print(0)
+        exit()
+
+    L = n // 2
     res = 0
-    k = 5
-    while n:
-        n //= k
-        res += 1
+    while L:
+        res += L // 5
+        L //= 5
     print(res)
 
 
