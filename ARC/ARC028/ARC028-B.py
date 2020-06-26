3d7a7275761c088d7707cdff41de9d6e9ae0fff1
HPP <happa_iidx@yahoo.co.jp>
2020-06-26 20:59:47 +0900

    簡潔なコードに変更

--- ARC/ARC028/ARC028-B.py
+++ ARC/ARC028/ARC028-B.py
@@ -1,8 +1,7 @@
-# https://atcoder.jp/contests/arc028/submissions/14192508
+# https://atcoder.jp/contests/arc028/submissions/14695720
 # B - 特別賞
 import sys
-from heapq import heapreplace, heappush
-
+from heapq import heappush, heapreplace
 sys.setrecursionlimit(10 ** 7)
 input = sys.stdin.readline
 f_inf = float('inf')
@@ -11,25 +10,19 @@ mod = 10 ** 9 + 7
 
 def resolve():
     n, k = map(int, input().split())
-    x = list(map(int, input().split()))
+    X = list(map(int, input().split()))
 
-    que = []
+    rank = []
     for i in range(k):
-        heappush(que, [-x[i], i + 1])
+        heappush(rank, [-X[i], i + 1])
 
-    kth_age, Rank = que[0]
-    print(Rank)
-    kth_age *= -1
-
+    print(rank[0][1])
     for i in range(k, n):
-        if x[i] < kth_age:
-            heapreplace(que, [-x[i], i + 1])
-            kth_age, Rank = que[0]
-            print(Rank)
-            kth_age *= -1
+        if X[i] < rank[0][0] * (-1):
+            heapreplace(rank, [-X[i], i + 1])
+            print(rank[0][1])
         else:
-            _, Rank = que[0]
-            print(Rank)
+            print(rank[0][1])
 
 
 if __name__ == '__main__':
