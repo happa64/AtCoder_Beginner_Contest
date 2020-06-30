b5a2048b39000ac9e83aba2709fb639bfc08d8bb
HPP <happa_iidx@yahoo.co.jp>
2020-06-30 14:06:24 +0900

    init commit

---
+++ Unrated/CODE_FESTIVAL_2015_Qual_B/CODE_FESTIVAL_2015_Qual_B-A.py
@@ -0,0 +1,16 @@
+# https://atcoder.jp/contests/code-festival-2015-qualb/submissions/14857992
+# A - ダブル文字列
+import sys
+
+sys.setrecursionlimit(10 ** 7)
+f_inf = float('inf')
+mod = 10 ** 9 + 7
+
+
+def resolve():
+    s = input()
+    print(s * 2)
+
+
+if __name__ == '__main__':
+    resolve()
