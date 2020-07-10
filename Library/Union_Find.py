e978fc5d0bda2916293e8cc393d850da6c42a0ca
HPP <happa_iidx@yahoo.co.jp>
2020-07-10 20:12:30 +0900

    docstringを追記

--- Library/Union_Find.py
+++ Library/Union_Find.py
@@ -1,19 +1,22 @@
-# Union Find（経路圧縮有）
 class UnionFind:
     def __init__(self, n):
         self.n = n
         self.parents = [-1] * n
 
-    # 親が同じか判別
     def find(self, x):
+        """
+        親が同じか判別する
+        """
         if self.parents[x] < 0:
             return x
         else:
             self.parents[x] = self.find(self.parents[x])
             return self.parents[x]
 
-    # 根を繋ぎ直す
     def union(self, x, y):
+        """
+        yをxの根に繋ぐ
+        """
         x = self.find(x)
         y = self.find(y)
         if x == y:
@@ -23,12 +26,16 @@ class UnionFind:
         self.parents[x] += self.parents[y]
         self.parents[y] = x
 
-    # 親が同じか判別
     def same(self, x, y):
+        """
+        xとyが同じ連結成分か判別する
+        """
         return self.find(x) == self.find(y)
 
-    # 連結成分の大きさを返す
     def size(self, x):
+        """
+        xの連結成分の大きさを返す
+        """
         return -self.parents[self.find(x)]
 
 
