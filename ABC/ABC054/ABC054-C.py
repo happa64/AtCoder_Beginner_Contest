1f742c4937f9d67fcae64d5ad1121c7731f344a0
HPP <happa_iidx@yahoo.co.jp>
2020-07-14 09:59:25 +0900

    bit DPの解法を追記

--- ABC/ABC054/ABC054-C.py
+++ ABC/ABC054/ABC054-C.py
@@ -1,4 +1,4 @@
-# https://atcoder.jp/contests/abc054/submissions/14856316
+# https://atcoder.jp/contests/abc054/submissions/15232421
 # C - One-stroke Path
 import sys
 from itertools import permutations
@@ -10,6 +10,9 @@ mod = 10 ** 9 + 7
 
 
 def resolve():
+    """
+    順列を全部試す。計算量O(N!)
+    """
     n, m = map(int, input().split())
     tree = [[] for _ in range(n)]
     for _ in range(m):
@@ -28,5 +31,43 @@ def resolve():
     print(res)
 
 
+def resolve2():
+    """
+    bit DP。計算量O(2^N*N^2)
+    """
+    n, m = map(int, input().split())
+    edge = [[f_inf] * n for _ in range(n)]
+    for _ in range(m):
+        a, b = map(int, input().split())
+        edge[a - 1][b - 1] = 1
+        edge[b - 1][a - 1] = 1
+
+    dp = [[f_inf] * n for _ in range(1 << n)]
+    dp[0][0] = 0
+    cnt = [[0] * n for _ in range(1 << n)]
+    cnt[0][0] = 1
+    for S in range(1 << n):
+        for v in range(n):
+            for u in range(n):
+                if v == u or S & (1 << v) or edge[u][v] == f_inf:
+                    continue
+                if dp[S | (1 << v)][v] > dp[S][u] + edge[u][v]:
+                    dp[S | (1 << v)][v] = dp[S][u] + edge[u][v]
+                    cnt[S | (1 << v)][v] = cnt[S][u]
+                elif dp[S | (1 << v)][v] == dp[S][u] + edge[u][v]:
+                    cnt[S | (1 << v)][v] += cnt[S][u]
+
+    mi = min(dp[-2])
+    idx = []
+    for i in range(n):
+        if dp[-2][i] == mi:
+            idx.append(i)
+
+    res = 0
+    for j in idx:
+        res += cnt[-2][j]
+    print(res)
+
+
 if __name__ == '__main__':
-    resolve()
+    resolve2()
