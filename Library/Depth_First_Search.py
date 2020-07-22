6765fe2fe36da463c589d890f7f6a222b582d2e3
HPP <happa_iidx@yahoo.co.jp>
2020-07-22 22:16:34 +0900

    init commit

--- Library/Depth_First_Search.py
+++ Library/Depth_First_Search.py
@@ -1,1 +1,18 @@
-# グラフ深さ優先探索
+color = [-1] * n
+def dfs(v, graph, cur=0):
+    """
+    二部グラフ判定
+    :param v: 始点
+    :param graph: 判定したいグラフ
+    :param cur: 塗る色。初期値は0
+    :return: 二部グラフであれば真を返す
+    """
+    color[v] = cur
+    for u in edge[v]:
+        if color[u] != -1:
+            if color[u] == cur:
+                return False
+            continue
+        if not dfs(u, graph, 1 - cur):
+            return False
+    return True
