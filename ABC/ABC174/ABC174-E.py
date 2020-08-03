9ee8ce09e14ef33b3e19e75bd903080c0aae934a
HPP <happa_iidx@yahoo.co.jp>
2020-08-03 11:52:27 +0900

    二分探索の条件を修正

--- ABC/ABC174/ABC174-E.py
+++ ABC/ABC174/ABC174-E.py
@@ -1,7 +1,6 @@
-# https://atcoder.jp/contests/abc174/submissions/15639798
+# https://atcoder.jp/contests/abc174/submissions/15656549
 # E - Logs
 import sys
-import math
 
 sys.setrecursionlimit(10 ** 7)
 input = sys.stdin.readline
@@ -11,14 +10,8 @@ mod = 10 ** 9 + 7
 
 def resolve():
     def meguru_bisect(ok, ng):
-        """
-        is_okを定義して下さい
-        :param ok: 取りうる最小の値-1
-        :param ng: 取りうる最大の値+1
-        :return: is_okを満たす最小(もしくは最大)の値
-        """
-        while abs(ok - ng) > 10 ** -5:
-            mid = (ok + ng) / 2
+        while abs(ok - ng) > 1:
+            mid = (ok + ng) // 2
             if is_ok(mid):
                 ok = mid
             else:
@@ -30,14 +23,13 @@ def resolve():
         for i in range(n):
             if A[i] > x:
                 cnt += (A[i] + x - 1) // x - 1
-
         return cnt <= k
 
     n, k = map(int, input().split())
     A = list(map(int, input().split()))
 
     res = meguru_bisect(10 ** 9 + 1, 0)
-    print(math.ceil(res))
+    print(res)
 
 
 if __name__ == '__main__':
