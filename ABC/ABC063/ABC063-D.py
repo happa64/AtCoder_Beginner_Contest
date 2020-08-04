254e32d6867ff250926392f21932c1d2a30219f4
HPP <happa_iidx@yahoo.co.jp>
2020-08-04 18:23:19 +0900

    init commit

---
+++ ABC/ABC063/ABC063-D.py
@@ -0,0 +1,44 @@
+# https://atcoder.jp/contests/abc063/submissions/15684767
+# D - Widespread
+import sys
+
+sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
+f_inf = float('inf')
+mod = 10 ** 9 + 7
+
+
+def resolve():
+    def meguru_bisect(ok, ng):
+        """
+        is_okを定義して下さい
+        :param ok: 取りうる最小の値-1
+        :param ng: 取りうる最大の値+1
+        :return: is_okを満たす最小(もしくは最大)の値
+        """
+        while abs(ok - ng) > 1:
+            mid = (ok + ng) // 2
+            if is_ok(mid):
+                ok = mid
+            else:
+                ng = mid
+        return ok
+
+    def is_ok(x):
+        cnt = 0
+        for h in H:
+            h -= b * x
+            if h > 0:
+                cnt += (h + ab - 1) // ab
+        return cnt <= x
+
+    n, a, b = map(int, input().split())
+    ab = a - b
+    H = list(int(input()) for _ in range(n))
+
+    res = meguru_bisect(10 ** 9 + 1, 0)
+    print(res)
+
+
+if __name__ == '__main__':
+    resolve()
