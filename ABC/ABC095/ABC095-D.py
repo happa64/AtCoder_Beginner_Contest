3e9292276a66870a5bf4c4437084cf31e1582ddc
HPP <happa_iidx@yahoo.co.jp>
2020-08-23 15:05:03 +0900

    より高速な解法を追記

--- ABC/ABC095/ABC095-D.py
+++ ABC/ABC095/ABC095-D.py
@@ -83,9 +83,7 @@ ide_ele = -f_inf
 
 
 def resolve():
-    n, c = map(int, input().split())
-    XV = [list(map(int, input().split())) for _ in range(n)]
-
+    """ 累積和 + セグ木：O(NlogN) """
     R = [0]
     prev = 0
     for i in range(n):
@@ -120,5 +118,37 @@ def resolve():
     print(max(res1, res2, res3, res4))
 
 
+def resolve2():
+    """ 累積和 + 最大値を前処理で求めておく：O(N) """
+    R1, R2 = [0], [0]
+    max1, max2 = [0], [0]
+    ma = 0
+    prev = 0
+    for x, y in XY:
+        R1.append(R1[-1] + y - (x - prev))
+        prev = x
+        if ma < R1[-1]:
+            ma = R1[-1]
+        max1.append(ma)
+
+    ma = 0
+    prev = 0
+    for x, y in XY[::-1]:
+        R2.append(R2[-1] + y - ((c - x) - prev))
+        prev = (c - x)
+        if ma < R2[-1]:
+            ma = R2[-1]
+        max2.append(ma)
+
+    res = max(max(R1), max(R2))
+    for i in range(1, n + 1):
+        point1 = R1[i] - XY[i - 1][0] + max2[n - i]
+        point2 = R2[i] - (c - XY[-i][0]) + max1[n - i]
+        res = max(res, point1, point2)
+    print(res)
+
+
 if __name__ == '__main__':
-    resolve()
+    n, c = map(int, input().split())
+    XY = [list(map(int, input().split())) for _ in range(n)]
+    resolve2()
