0953127d6a63171e97f7ba7bd4142a2e0ca31a6b
HPP <happa_iidx@yahoo.co.jp>
2020-09-03 16:30:17 +0900

    ダイクストラ法による解法を追記

--- ARC/NIKKEI_2019_2nd_Qual/NIKKEI_2019_2nd_Qual-D.py
+++ ARC/NIKKEI_2019_2nd_Qual/NIKKEI_2019_2nd_Qual-D.py
@@ -1,11 +1,39 @@
-# https://atcoder.jp/contests/nikkei2019-2-qual/submissions/16478910
+# https://atcoder.jp/contests/nikkei2019-2-qual/submissions/16479159
 # D - Shortest Path on a Line
 import sys
+from heapq import heapify, heappop, heappush
 
 sys.setrecursionlimit(10 ** 7)
 input = sys.stdin.readline
 f_inf = float('inf')
 mod = 10 ** 9 + 7
+
+
+def dijkstra_heap(v, start, edge):
+    """
+    ダイクストラ法
+    :param v: 走査するグラフの頂点数
+    :param start: 始点
+    :param edge: 重み付き隣接リスト。[cost, node]の形で渡す
+    :return: 始点から各頂点への最短距離
+    """
+    res = [f_inf] * v
+    res[start] = 0
+    checked = [False] * v
+    checked[start] = True
+    edge_list = []
+    for u in edge[start]:
+        heappush(edge_list, u)
+    while len(edge_list):
+        min_edge = heappop(edge_list)
+        cost, node = min_edge
+        if not checked[node]:
+            res[node] = cost
+            checked[node] = True
+            for next_cost, next_node in edge[node]:
+                if not checked[next_node]:
+                    heappush(edge_list, [next_cost + cost, next_node])
+    return res
 
 
 class SegTree:
@@ -85,5 +113,19 @@ def resolve():
     print(res if res != f_inf else -1)
 
 
+def resolve2():
+    n, m = map(int, input().split())
+    edge = [[] for _ in range(n)]
+    for _ in range(m):
+        l, r, c = map(int, input().split())
+        edge[l - 1].append([c, r - 1])
+        edge[r - 1].append([c, l - 1])
+    for i in reversed(range(1, n)):
+        edge[i].append([0, i - 1])
+
+    dist = dijkstra_heap(n, 0, edge)
+    print(-1 if dist[-1] == f_inf else dist[-1])
+
+
 if __name__ == '__main__':
-    resolve()
+    resolve2()
