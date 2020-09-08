639379a2e5be084ef9f766b2e7b37e4c1269c67f
HPP <happa_iidx@yahoo.co.jp>
2020-09-08 16:06:38 +0900

    init commit

---
+++ Library/Floor_Sum.py
@@ -0,0 +1,15 @@
+def floor_sum(n, m, a, b):
+    res = 0
+    if a >= m:
+        res += (n - 1) * n * (a // m) // 2
+        a %= m
+    if b >= m:
+        res += n * (b // m)
+        b &= m
+    y_max = (a * n + b) // m
+    x_max = (y_max * m - b)
+    if y_max == 0:
+        return res
+    res += (n - (x_max + a - 1) // a) * y_max
+    res += floor_sum(y_max, a, m, (a - x_max % a) % a)
+    return res
