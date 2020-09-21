e800819f81d4ef9d6a94baa6d87e80640621a781
HPP <happa_iidx@yahoo.co.jp>
2020-09-21 17:37:30 +0900

    init commit

--- ARC/ARC048/ARC048-B.py
+++ ARC/ARC048/ARC048-B.py
@@ -1,4 +1,4 @@
-# https://atcoder.jp/contests/arc048/submissions/15275732
+# https://atcoder.jp/contests/arc048/submissions/16934563
 # B - AtCoderでじゃんけんを
 import sys
 from itertools import accumulate
@@ -11,31 +11,33 @@ mod = 10 ** 9 + 7
 
 def resolve():
     n = int(input())
-    RH = [list(map(int, input().split())) for _ in range(n)]
+    RH = [list(map(lambda x:int(x) - 1, input().split())) for _ in range(n)]
+    MAX_RATE = max([r for r, _ in RH]) + 1
 
-    MAX_RATE = 10 ** 5 + 1
-    rate = [[0] * 3 for _ in range(MAX_RATE)]
-    rate_cnt = [0] * MAX_RATE
+    hand_by_rate = [[0] * MAX_RATE for _ in range(3)]
+    rate_distribution = [0] * MAX_RATE
     for r, h in RH:
-        rate_cnt[r] += 1
-        rate[r][h - 1] += 1
-    acc_rate_cnt = list(accumulate(rate_cnt))
+        hand_by_rate[h][r] += 1
+        rate_distribution[r] += 1
+
+    rate_distribution = [0] + list(accumulate(rate_distribution))
 
     res = [[0] * 3 for _ in range(n)]
     for i in range(n):
         r, h = RH[i]
-        g, c, p = rate[r]
-        win = c if h == 1 else p if h == 2 else g
-        lose = p if h == 1 else g if h == 2 else c
-        draw = g - 1 if h == 1 else c - 1 if h == 2 else p - 1
-        win += acc_rate_cnt[r - 1]
-        lose += acc_rate_cnt[MAX_RATE - 1] - acc_rate_cnt[r]
-        res[i][0] += win
-        res[i][1] += lose
+        win = rate_distribution[r]
+        lose = rate_distribution[-1] - rate_distribution[r + 1]
+        draw = hand_by_rate[h][r] - 1
+
+        win += hand_by_rate[1][r] if h == 0 else hand_by_rate[2][r] if h == 1 else hand_by_rate[0][r]
+        lose += hand_by_rate[2][r] if h == 0 else hand_by_rate[0][r] if h == 1 else hand_by_rate[1][r]
+
+        res[i][0] = win
+        res[i][1] = lose
         res[i][2] = draw
 
-    for i in res:
-        print(*i)
+    for i in range(n):
+        print(*res[i])
 
 
 if __name__ == '__main__':
