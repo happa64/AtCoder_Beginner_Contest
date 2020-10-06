7e91c3ac579c0ff97761a5a020ff7de3c028dc00
HPP <happa_iidx@yahoo.co.jp>
2020-10-06 13:44:00 +0900

    n元に対応

--- Library/CRT.py
+++ Library/CRT.py
@@ -1,14 +1,16 @@
-def CRT(b1, m1, b2, m2):
+def CRT(B, M):
     def extGcd(a, b):
         if a == 0:
             return b, 0, 1
         g, y, x = extGcd(b % a, a)
         return g, x - (b // a) * y, y
 
-    d, p, q = extGcd(m1, m2)
-    if (b2 - b1) % d != 0:
-        return 0, -1
-    m = m1 * (m2 // d)
-    tmp = (b2 - b1) // d * p % (m2 // d)
-    r = (b1 + m1 * tmp) % m
-    return r, m
+    b1, m1 = 0, 1
+    for b2, m2 in zip(B, M):
+        d, p, q = extGcd(m1, m2)
+        if (b2 - b1) % d != 0:
+            return 0, -1
+        tmp = (b2 - b1) // d * p % (m2 // d)
+        b1 += m1 * tmp
+        m1 *= m2 // d
+    return b1 % m1, m1
