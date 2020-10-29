f8ca766f9fb8386ff0433a16f244c43e36c090f7
HPP <happa_iidx@yahoo.co.jp>
2020-10-29 20:42:08 +0900

    init commit

---
+++ Unrated/Code_Formula_2014_Qual_B/Code_Formula_2014_Qual_B-A.py
@@ -0,0 +1,16 @@
+# https://atcoder.jp/contests/code-formula-2014-qualb/submissions/17718407
+# A - サイコロ
+import sys
+
+sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
+f_inf = float('inf')
+mod = 10 ** 9 + 7
+
+
+def resolve():
+    print(7 - int(input()))
+
+
+if __name__ == '__main__':
+    resolve()
