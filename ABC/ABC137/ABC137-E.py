c3cefdedb80df4b0eb5d43a46fabb7106a97cc23
HPP <happa_iidx@yahoo.co.jp>
2020-11-26 18:20:11 +0900

    init commit

--- ABC/ABC137/ABC137-E.py
+++ ABC/ABC137/ABC137-E.py
@@ -1,8 +1,6 @@
-# https://atcoder.jp/contests/abc134/submissions/15079142
-# E - Sequence Decomposing
+# https://atcoder.jp/contests/abc137/submissions/18417495
+# E - Coins Respawn
 import sys
-from collections import deque
-from bisect import bisect_left
 
 sys.setrecursionlimit(10 ** 7)
 input = sys.stdin.readline
@@ -11,20 +9,47 @@ mod = 10 ** 9 + 7
 
 
 def resolve():
-    n = int(input())
-    A = list(int(input()) for _ in range(n))
+    def dfs(v):
+        for _, u in edge[v]:
+            if visited[u]:
+                continue
+            visited[u] = True
+            dfs(u)
 
-    LIS = deque()
+    n, m, p = map(int, input().split())
+    dist = [[-f_inf] * n for _ in range(n)]
+    for _ in range(m):
+        a, b, c = map(int, input().split())
+        dist[a - 1][b - 1] = max(dist[a - 1][b - 1], c - p)
+
+    edge = [[] for _ in range(n)]
     for i in range(n):
-        if len(LIS) == 0:
-            LIS.appendleft(A[i])
-        else:
-            idx = bisect_left(LIS, A[i])
-            if idx == 0:
-                LIS.appendleft(A[i])
-            else:
-                LIS[idx - 1] = A[i]
-    print(len(LIS))
+        for j in range(n):
+            if dist[i][j] != -f_inf:
+                edge[i].append([-dist[i][j], j])
+
+    score = [f_inf] * n
+    score[0] = 0
+    for _ in range(n):
+        update = [False] * n
+        for v in range(n):
+            for d, u in edge[v]:
+                if score[v] != f_inf and score[v] + d < score[u]:
+                    score[u] = score[v] + d
+                    update[u] = True
+
+    visited = [False] * n
+    for i in range(n):
+        if visited[i]:
+            continue
+        if update[i]:
+            visited[i] = True
+            dfs(i)
+
+    if visited[-1]:
+        print(-1)
+    else:
+        print(max(0, score[-1] * -1))
 
 
 if __name__ == '__main__':
