b93365f1b2caf2346098cf0768f2e37e0d8e6cff
HPP <happa_iidx@yahoo.co.jp>
2020-12-10 20:42:32 +0900

    init commit

---
+++ ABC/TENKA1_Beginner_2017/TENKA1_2017-A.py
@@ -0,0 +1,16 @@
+# https://atcoder.jp/contests/tenka1-2017-beginner/submissions/13608496
+# A - Accepted...?
+import sys
+
+sys.setrecursionlimit(10 ** 7)
+f_inf = float('inf')
+mod = 10 ** 9 + 7
+
+
+def resolve():
+    s = input()
+    print(s.count("1"))
+
+
+if __name__ == '__main__':
+    resolve()
