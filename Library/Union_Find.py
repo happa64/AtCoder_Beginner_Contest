c8821e51f9311fefd6c3fbdb0759e9a83417f77b
HPP <happa_iidx@yahoo.co.jp>
2021-02-11 17:38:19 +0900

    重み付きUnion-Findにアップグレード

--- Library/Union_Find.py
+++ Library/Union_Find.py
@@ -1,40 +1,47 @@
 class UnionFind:
     def __init__(self, n):
         self.n = n
-        self.parents = [-1] * n
+        self.par = [-1] * n
+        self.diff_weight = [0] * n
 
     def find(self, x):
-        """xの親を返す"""
-        if self.parents[x] < 0:
+        if self.par[x] < 0:
             return x
         else:
-            self.parents[x] = self.find(self.parents[x])
-            return self.parents[x]
+            y = self.find(self.par[x])
+            self.diff_weight[x] += self.diff_weight[self.par[x]]
+            self.par[x] = y
+            return y
 
-    def union(self, x, y):
-        """yをxの根に繋ぐ（マージテク有）"""
+    def union(self, x, y, w=0):
+        w += self.diff_weight[x] - self.diff_weight[y]
         x = self.find(x)
         y = self.find(y)
         if x == y:
-            return
-        if self.parents[x] > self.parents[y]:
-            x, y = y, x
-        self.parents[x] += self.parents[y]
-        self.parents[y] = x
+            return False
+        else:
+            if self.par[x] > self.par[y]:
+                x, y = y, x
+                w *= -1
+            self.par[x] += self.par[y]
+            self.par[y] = x
+            self.diff_weight[y] = w
+            return True
 
     def same(self, x, y):
-        """xとyが同じ連結成分か判別する"""
         return self.find(x) == self.find(y)
 
     def size(self, x):
-        """xの連結成分の大きさを返す"""
-        return -self.parents[self.find(x)]
+        return -self.par[self.find(x)]
+
+    def weight(self, x):
+        self.find(x)
+        return self.diff_weight[x]
+
+    def diff(self, x, y):
+        return self.weight(y) - self.weight(x)
 
     def kruskal(self, edge):
-        """
-        :param edge: edge = [(コスト, 頂点1, 頂点2),...]の形で重み付き隣接リストを渡して下さい
-        :return: 最小全域木のコストの和
-        """
         edge.sort()
         cost_sum = 0
         for cost, node1, node2 in edge:
