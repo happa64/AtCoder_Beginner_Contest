fb65fda29a7f8572010b85019b7b96e8a137175e
HPP <happa_iidx@yahoo.co.jp>
2021-03-19 20:46:28 +0900

    init commit

--- ABC/ABC172/ABC172-E.py
+++ ABC/ABC172/ABC172-E.py
@@ -1,26 +1,22 @@
+# https://atcoder.jp/contests/abc172/submissions/21035192
+# E - NEQ
 import sys
 
 sys.setrecursionlimit(10 ** 7)
 input = sys.stdin.readline
 f_inf = float('inf')
-mod = 10 ** 9 + 7
+MOD = 10 ** 9 + 7
 
 
 class CmbMod:
     def __init__(self, n, p):
-        """
-        二項係数nCr(n個の区別できるものからr個のものを選ぶ組み合わせの数)をpで割った余りを求める
-        """
         self.n = n
         self.p = p
         self.fact = [1, 1]
-        self.factinv = [1, 1]
+        self.fact_inv = [1, 1]
         self.inv = [0, 1]
 
-    def cmb_mod(self, n, r):
-        """
-        二項係数nCr(mod p)をO(r)にて計算。nが大きいがrは小さい時に使用。
-        """
+    def calc_cmb_mod(self, n, r):
         numer, denom = 1, 1
         for i in range(r):
             numer = (numer * (n - i)) % self.p
@@ -28,35 +24,34 @@ class CmbMod:
         return (numer * pow(denom, self.p - 2, self.p)) % self.p
 
     def prep(self):
-        """
-        二項係数nCr(mod p)をO(1)で求める為の前処理をO(N)にて実行。
-        """
         for i in range(2, self.n + 1):
             self.fact.append((self.fact[-1] * i) % self.p)
             self.inv.append((-self.inv[self.p % i] * (self.p // i)) % self.p)
-            self.factinv.append((self.factinv[-1] * self.inv[-1]) % self.p)
+            self.fact_inv.append((self.fact_inv[-1] * self.inv[-1]) % self.p)
 
-    def cmb_mod_with_prep(self, n, r):
-        """
-        二項係数nCr(mod p)をO(1)で求める。事前にprepを実行する事。
-        """
-        if (r < 0) or (n < r):
+    def fact_mod(self, n, r):
+        return self.fact[n] * self.fact_inv[n - r] % self.p
+
+    def cmb_mod(self, n, r):
+        if r < 0 or n < r:
             return 0
         r = min(r, n - r)
-        return self.fact[n] * self.factinv[r] % self.p * self.factinv[n - r] % self.p
+        return self.fact[n] * self.fact_inv[r] % self.p * self.fact_inv[n - r] % self.p
 
 
 def resolve():
     n, m = map(int, input().split())
 
-    cmb = CmbMod(n + m, mod)
+    cmb = CmbMod(m, MOD)
     cmb.prep()
-    fact = [1]
-    for i in range(2, m + 1):
-        fact.append(fact[-1] * i % mod)
 
-
-
+    res = 0
+    for k in range(n + 1):
+        now = cmb.cmb_mod(n, k) * cmb.fact_mod(m, n) % MOD * cmb.fact_mod(m - k, n - k) % MOD
+        if k % 2:
+            now = -now
+        res = (res + now) % MOD
+    print(res)
 
 
 if __name__ == '__main__':
