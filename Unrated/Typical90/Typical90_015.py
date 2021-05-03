ded4e60a3c54e59bbfac9030368a4ebeed11af86
HPP <happa_iidx@yahoo.co.jp>
2021-05-03 10:38:28 +0900

    init commit

--- Unrated/Typical90/Typical90_015.py
+++ Unrated/Typical90/Typical90_015.py
@@ -1,3 +1,13 @@
+# https://atcoder.jp/contests/typical90/submissions/22272096
+# 015 - Don't be too close（★6）
+import sys
+
+sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
+f_inf = float('inf')
+MOD = 10 ** 9 + 7
+
+
 class CmbMod:
     def __init__(self, n, p, flg=True):
         self.n = n
@@ -29,3 +39,19 @@ class CmbMod:
             return 0
         r = min(r, n - r)
         return self.fact[n] * self.fact_inv[r] % self.p * self.fact_inv[n - r] % self.p
+
+
+def solve():
+    n = int(input())
+    cmb = CmbMod(n, MOD)
+    for k in range(1, n + 1):
+        res = 0
+        limit = (n + k - 1) // k + 1
+        for a in range(1, limit):
+            res += cmb.cmb_mod(n - (a - 1) * (k - 1), a)
+            res %= MOD
+        print(res)
+
+
+if __name__ == '__main__':
+    solve()
